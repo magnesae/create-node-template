573924b9b522ddef12194350cb4832d7e8095885
magnesae <opaduel@hotmail.com>
2023-11-01 15:17:06 +0900

    - add express-typescript-jest-zod

---
+++ src/templates/express-typescript-jest-zod/src/apis/example/exampleService.ts
@@ -0,0 +1,23 @@
+import "dotenv/config";
+import * as ExampleRepository from "./exampleRepository.js";
+
+/**** GET *****/
+
+export const getExample = async (): Promise<string> => {
+  return "Hello world from /example";
+};
+export const getAddressExample = async (
+  exampleAddress: string,
+): Promise<string> => {
+  return `${exampleAddress} is a valid address`;
+};
+
+export const getExampleIds = async (exampleId: number): Promise<object> => {
+  const tokenIds = await ExampleRepository.getExampleIds(exampleId);
+
+  return tokenIds;
+};
+
+/**** POST *****/
+
+/**** HELPER *****/
